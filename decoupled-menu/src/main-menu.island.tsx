68ef4fcad57ae65438f9f529b5e8e06fe48d0015
jdwjdwjdw <jacobdeanwilson@gmail.com>
2024-04-03 00:26:58 +0000

    Remove commented out code

--- decoupled-menu/src/main-menu.island.tsx
+++ decoupled-menu/src/main-menu.island.tsx
@@ -33,7 +33,6 @@ const MenuWrapper = styled.div<{ open?: boolean }>`
 
 const TopList = styled.ul`
   flex-wrap: wrap;
-  // justify-content: flex-end;
   justify-content: flex-start;
   list-style: none;
   margin: 0;
@@ -43,31 +42,20 @@ const TopList = styled.ul`
 
   @media (min-width: 992px) {
     display: flex;
-    // justify-content: flex-start;
     background: transparent;
     padding: 0;
     font-size: 19px;
     height: 100%;
     width: 100%;
-    // container-type: size;
-
-  //   @container (min-height: 200px) {
-  //     a {
-  //       padding: 1.6rem 0;
-  //     }
-  //   }
-  // }
 `
 
 const MobileMenuButton = styled.button`
   position: absolute;
-  // top: -60px;
   top: -70px;
   right: 10px;
   box-shadow: none;
   background: transparent;
   border: 0;
-  // border-bottom: 1px solid transparent;
   color: #2e2d29;
   padding: 0;
   display: flex;
@@ -76,7 +64,6 @@ const MobileMenuButton = styled.button`
   font-size: 1.6rem;
 
   &:hover, &:focus {
-    // border-bottom: 1px solid #2e2d29;
     background: transparent;
     color: #2e2d29;
     box-shadow: none;
@@ -231,17 +218,11 @@ const Button = styled.button`
   }
 
   @media (min-width: 992px) {
-    // color: #b1040e;
     color: #2e2d29;
     background: transparent;
     border-radius: 0;
-    // align-self: center;
     width: auto;
     margin-left: 1rem;
-    // position: ${({level}) => level != 0 ? "absolute" : "unset"};
-    // position: ${props => props.level != 0 ? "absolute" : "relative"};
-    // right: ${props => props.level != 0 ? "2rem" : "unset"};
-    // right: ${({level}) => level != 0 ? "2rem" : "unset"};
 
     &:hover, &:focus {
       border-bottom: 2px solid #2e2d29;
@@ -264,7 +245,6 @@ const MenuItemContainer = styled.div<{ level?: number }>`
     margin-bottom: ${props => props.level === 0 ? "6px" : ""};
     height: 100%;
     margin-bottom: 0;
-    // align-items: end;
     align-items: unset;
   }
 `
@@ -286,53 +266,22 @@ const MenuLink = styled.a<{ isCurrent?: boolean, inTrail?: boolean, level?: numb
   }
 
   @media (min-width: 992px) {
-    // color: #b1040e;
     color: #2e2d29;
-    // padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "1.6rem 0"};
-    // padding: ${({level}) => level != 0 ? "16px" : "1.6rem 0"};
-    // padding: ${({level}) => level != 0 ? "3.6rem 3rem 3.6rem 3.6rem" : "1.6rem 0"};
     padding: ${({level}) => level != 0 ? "3.6rem 6.2rem 3.6rem 3.6rem" : "1.6rem 0"};
     padding-left: ${({level}) => level === 2 ? "5.6rem" : ""};
     padding-left: ${({level}) => level === 3 ? "7.6rem" : ""};
-    // padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "0 0 2rem 0"};
-    // margin-top: ${({level}) => level != 0 ? "16px 0 16px 16px" : "1.5rem"};
     border-bottom: ${({level, inTrail, isCurrent}) => level === 0 ? (isCurrent ? "6px solid #007C7E" : (inTrail ? "6px solid #b6b1a9" : "6px solid transparent")) : ""};
-    // border-left: ${({level, isCurrent}) => level != 0 ? (isCurrent ? "6px solid #007C7E" : "6px solid transparent") : "none"};
-    // border-left: ${({level, isCurrent}) => level != 0 ? (isCurrent ? "6px solid #007C7E" : "none") : "none"};
     border-left: ${({level, isCurrent}) => level != 0 ? (isCurrent ? "6px solid #007C7E" : "6px solid transparent") : "none"};
-    // margin-bottom: ${({level, inTrail, isCurrent}) => level === 0 ? (isCurrent ? "-6px" : (inTrail ? "-6px" : "-6px")) : ""};
-    // margin-bottom: 0;
-    // padding-top: 0;
-    // padding-bottom: 5.2rem;
     background-color: ${({level, isCurrent}) => level != 0 ? (isCurrent ? "#f4f4f4" : "none") : "none"};
 
     &:hover, &:focus {
       color: #2e2d29;
-      // border-left: ${({level}) => level != 0 ? "6px solid #2e2d29" : "none"};
-      // border-left: ${({level}) => level != 0 ? "6px solid #2e2d29" : "6px solid transparent"};
-      // border-left: none;
-      // border-left: ${({level}) => level != 0 ? "6px solid transparent" : "none"};
       border-left: ${({level, isCurrent}) => level != 0 ? (isCurrent ? "6px solid #007C7E" : "6px solid transparent") : "none"};
-      // background-color: #f4f4f4;
       background-color: ${({level}) => level != 0 ? "#f4f4f4" : "unset"};
     }
   }
 
-  // @media (min-width: 1200px) {
-  //   // padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "0 0 5.2rem 0"};
-  //   padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "0 0 4.7rem 0"};
-  // }
-
-  // @media (min-width: 1500px) {
-  //   // padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "0 0 5.2rem 0"};
-  //   padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "0 0 4.7rem 0"};
-  // }
-
   @media (min-width: 1396px) {
-    // padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "0 0 5.2rem 0"};
-    // padding: ${({level}) => level != 0 ? "16px 0 16px 16px" : "0 0 4.7rem 0"};
-    // padding: ${({level}) => level != 0 ? "16px" : "0 0 4.7rem 0"};
-    // padding: ${({level}) => level != 0 ? "3.6rem 3rem 3.6rem 3.6rem" : "0 0 4.7rem 0"};
     padding: ${({level}) => level != 0 ? "3.6rem 6.2rem 3.6rem 3.6rem" : "0 0 4.7rem 0"};
     padding-left: ${({level}) => level === 2 ? "5.6rem" : ""};
     padding-left: ${({level}) => level === 3 ? "7.6rem" : ""};
@@ -347,7 +296,6 @@ const NoLink = styled.span<{ level?: number }>`
   padding: 16px 0 16px 16px;
 
   @media (min-width: 992px) {
-    // color: #b1040e;
     color: #2e2d29;
     font-weight: 500;
     font-family: Roboto Slab;
@@ -371,7 +319,6 @@ const MenuList = styled.ul<{ open?: boolean, level?: number }>`
     background: #ffffff;
     border-top: 1px solid #d9d9d9;
     min-width: 370px;
-    // right: 0;
   }
 `
 
@@ -387,21 +334,7 @@ const ListItem = styled.li<{ level?: number }>`
 
   @media (min-width: 992px) {
     border-bottom: ${props => props.level === 0 ? "none" : "1px solid #d9d9d9"};
-    // padding: ${props => props.level > 0 ? "0 10px" : "0"};
     padding: 0;
-  }
-`
-
-const MenuItemDivider = styled.div`
-  width: 1px;
-  height: 20px;
-  margin: 0 6px;
-  background: #766253;
-  display: none;
-  flex-shrink: 0;
-
-  @media (min-width: 992px) {
-    display: block;
   }
 `
 
@@ -458,9 +391,6 @@ const MenuItem = ({title, url, items, expanded, level = 0}: { title: string, url
 
         {(items && expanded) &&
           <>
-            {/* {level === 0 &&
-              <MenuItemDivider/>
-            } */}
             <Button
               ref={buttonRef}
               onClick={() => setSubmenuOpen(!submenuOpen)}
@@ -468,7 +398,6 @@ const MenuItem = ({title, url, items, expanded, level = 0}: { title: string, url
               aria-label={(submenuOpen ? "Close" : "Open") + ` ${title} Submenu`}
               style={{
                 position: level != 0 ? "absolute" : "unset",
-                // right: level != 0 ? "2rem" : "unset",
                 right: level != 0 ? "4.2rem" : "unset",
                 top: level != 0 ? "2.2rem" : "unset",
               }}
